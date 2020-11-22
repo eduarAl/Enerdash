e3e0be9627eba04462515f53fdcde9a2ae7b055d
Eduardo <eduardo.espinoza10026@est.uca.edu.ni>
2020-11-21 23:16:32 -0600

    Se avanzó con el origen de datos.

---
+++ app/src/main/java/com/example/enerdash/Data/AppliancesFileSource.java
@@ -0,0 +1,46 @@
+package com.example.enerdash.Data;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+
+import com.google.gson.Gson;
+import com.google.gson.annotations.SerializedName;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
+public class AppliancesFileSource {
+    /*private final Gson parser;
+    private final Context mContext;
+    private static final String POINTS_FILE_NAME = "points.json";
+
+    public PointsAssetSource(@NonNull Context context) {
+        mContext = context;
+        parser = new Gson();
+    }
+
+    @Override
+    public List<PointModel> getAll(int count) {
+        String json = FileHelper.getJsonFromAssets(mContext, POINTS_FILE_NAME);
+        ListResult listResult = parser.fromJson(json, ListResult.class);
+        if(listResult == null) return null;
+        return filterByCount(listResult.list, count);
+    }
+
+    private List<PointModel> filterByCount(@NonNull List<PointModel> originalList, int count) {
+        if(count < 0) throw new IllegalArgumentException("Parametro count inválido");
+        if(count == 0) return originalList;
+        if(count >= originalList.size()) return originalList;
+        return originalList.subList(0, count);
+    }
+
+    static class ListResult {
+        @SerializedName("data")
+        List<PointModel> list;
+
+        public ListResult(List<PointModel> list) {
+            this.list = list;
+        }
+    }*/
+}
